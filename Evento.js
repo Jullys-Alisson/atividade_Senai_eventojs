de59849804017c4eb08ba52f482821a11ce50e2b
Jullys Alisson <jullysalisson10@gmail.com>
2021-10-13 15:51:48 -0300

    Código evento JS

---
+++ Evento.js
@@ -0,0 +1,23 @@
+let idade = 19
+let pessoas 
+let data = 15/10/2021
+
+if (data < 13/10/2021) {
+    console.log ("Cadastro não permitido, data ultrapassada")  
+}else {
+    console.log ("Cadastro permitido")
+}
+
+if (idade <18) {
+    console.log ("Cadastro não permitido, idade inferior a 18 anos")
+    } else {
+    console.log ("Cadastro permitido")
+}
+do {
+    console.log ("cadastrando",pessoas)
+    pessoas++
+} 
+while (pessoas <=100) {
+    console.log ("Cadastro permitido")
+}
+
